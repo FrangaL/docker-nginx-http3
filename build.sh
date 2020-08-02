b6820bae10a6ad8f2963b9f23073640e00dfc7dc
Son <frangal@gmail.com>
2020-08-02 21:09:20 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+VCS_REF=$(git rev-parse --short HEAD)
+
+echo "BUILD_DATE: ${BUILD_DATE}"
+echo "VCS_REF: ${VCS_REF}"
+
+printenv
+
+docker build --build-arg BUILD_DATE=${BUILD_DATE} \
+  --build-arg VCS_REF=${VCS_REF} \
+  -t $IMAGE_NAME .
